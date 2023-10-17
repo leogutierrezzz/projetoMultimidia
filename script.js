680b82f9bb2ad446ded5401ecc215a499f829bfa
Leonardo Gutierrez Medeiros <leogutierrezzz>
2023-10-17 17:51:07 -0400

    Melhorando a estrutura e adicionando novas section

--- script.js
+++ script.js
@@ -25,3 +25,15 @@ function start(){
 }
 
 window.addEventListener("load", start);
+
+window.sr = ScrollReveal({ reseat: true});
+
+sr.reveal('#produtos', { duration: 20000});
+sr.reveal('#descricao', { duration: 20000});
+sr.reveal('#videos', { duration: 20000});
+sr.reveal('#bloco-espresso', {rotate:{ x:0, y:80, z:0}, duration: 1000});
+sr.reveal('#bloco-cappuccino', {rotate:{ x:0, y:80, z:0}, duration: 5000});
+sr.reveal('#bloco-latte', {rotate:{ x:0, y:80, z:0}, duration: 10000});
+sr.reveal('#bloco-macchiato', {rotate:{ x:0, y:80, z:0}, duration: 15000});
+sr.reveal('#bloco-mocha', {rotate:{ x:0, y:80, z:0}, duration: 20000});
+sr.reveal('#bloco-brasileirinho', {rotate:{ x:0, y:80, z:0}, duration: 25000});
